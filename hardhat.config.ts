7261a026d875df12f7000542b950169a23b63579
hanoon-07 <muhhamedhanoon@gmail.com>
2026-03-07 21:28:10 +0530

    ts tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
 import { defineConfig } from "hardhat/config";
 
 export default defineConfig({
+  plugins: [hardhatToolboxViemPlugin],
   solidity: {
     version: "0.8.28",
   },
